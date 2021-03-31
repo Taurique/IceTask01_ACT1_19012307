b3b8ec2efff85aa63011998f3282fb8dbd920d2a
Moegamat Isaacs <19012307@vcconnect.co.za>
2021-03-31 20:37:18 +0200

    ICE 1 first change

---
+++ IceTask01_ACT1_19012307/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IceTask01_ACT1_19012307
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
